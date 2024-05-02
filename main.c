353c0d1fe6adea9e496c505f1a69ee65fc0dfcb0
blazing-blast <lukabijma1@gmail.com>
2024-05-02 16:52:20 +0200

    Added a single comment.

--- main.c
+++ main.c
@@ -13,19 +13,17 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     InitializeLib(ImageHandle, SystemTable);
 
     Status = Print(L"Powered on!\n");
-    if (EFI_ERROR(Status))
-    {
-        Print(L"Print() errored... %d", Status);
-    }
 
-    Status = uefi_call_wrapper(ST->ConOut->OutputString, 1, L"Hello from the wrapper!\n");
-
+    // Attempt to find out where we are loaded in memory, so I can hook up a debugger to it.
+    // Sadly it generatates an exception: Type - 06(#UD - Invalid Opcode) with RIP = 0xB0000.
     Status = uefi_call_wrapper(BS->HandleProtocol,
                                3,
                                ImageHandle,
                                &LoadedImageProtocol,
                                &loaded_image);
-    Print(L"Loaded image!\n"); // This will never be reached
+
+    Print(L"Loaded image!\n");
+
     if (EFI_ERROR(Status))
     {
         Print(L"handleprotocol: %r\n", Status);
